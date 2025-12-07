27ae9fe67d0a497d6c453a56e5c27a2a580968ca
Aryan <aryanshandilya10@gmail.com>
2025-12-07 19:51:15 +0530

    setup: init express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello there");
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello there")
+});
+
+app.listen(port, () => console.log(`Hello there server is running on http://localhost:${port}`));
