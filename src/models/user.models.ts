773e8a3bf57dc91cab0fffaf08233c7ced34d06b
Aryan <aryanshandilya10@gmail.com>
2025-12-09 20:30:11 +0530

    added user mongoose hooks

--- src/models/user.models.ts
+++ src/models/user.models.ts
@@ -1,46 +1,61 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
-  avatar: {
-    type: { url: String, localPath: String },
-    default: {
-      url: "https://avatars.githubusercontent.com/u/64391491?s=400&u=9ffe76c447275d73fddcd4216ccde1d0ca6291e8&v=4",
-      localPath: "",
-  }},
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true,
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: { url: String, localPath: String },
+      default: {
+        url: "https://avatars.githubusercontent.com/u/64391491?s=400",
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: String,
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+    emailVerificationToken: String,
+    emailVerificationExpiry: Date,
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true
-  },
-  fullName: {
-    type: String,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    select: false
-  },
-  isEmailVerified: {
-    type: Boolean,
-    default: false
-  },
-  refreshToken: String,
-  forgotPasswordToken: String,
-  forgotPasswordExpiry: Date,
-  emailVerificationToken: String,
-  emailVerificationExpiry: Date
-}, { timestamps: true });
+  { timestamps: true },
+);
 
-export const Usermodel = mongoose.model("User", userSchema);
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (password: string) {
+  return bcrypt.compare(password, this.password);
+};
+
+export const UserModel = mongoose.model("User", userSchema);
