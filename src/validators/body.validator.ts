1736d309860c2bf91158811529ca971de574e896
Aryan <aryanshandilya10@gmail.com>
2025-12-13 13:22:40 +0530

    added body validator

---
+++ src/validators/body.validator.ts
@@ -0,0 +1,21 @@
+import z from "zod";
+
+const strongPasswordRegex = new RegExp(
+  "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+);
+
+export const bodySchema = z.object({
+  username: z
+    .string()
+    .min(5, "Username must be atleast 5 characters")
+    .toLowerCase()
+    .trim(),
+  password: z
+    .string()
+    .min(8, "Password must be atleast 8 characters long")
+    .regex(strongPasswordRegex, {
+      error:
+        "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+    }),
+  email: z.email("Please enter a valid email address"),
+});
