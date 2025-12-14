c4128dee371c4e220a4431165ce94b08d6754c37
Aryan <aryanshandilya10@gmail.com>
2025-12-14 12:52:05 +0530

    extracted issueTokenForUser to services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,24 +1,9 @@
-import { UserModel } from "../models";
-import { ApiError } from "../utils";
+import { type IUser } from "../models";
 
-export const generateAccessandRefreshToken = async (userId: string) => {
-  try {
-    const user = await UserModel.findById(userId);
-    if (!user) {
-      throw new ApiError({
-        statuscode: 404,
-        message: "User not found",
-      });
-    }
-    const accessToken = user.generateAccessToken();
-    const refreshToken = user.generateRefreshToken();
-    user.refreshToken = refreshToken;
-    await user.save({ validateBeforeSave: false });
-    return { accessToken, refreshToken };
-  } catch (error) {
-    throw new ApiError({
-      statuscode: 500,
-      message: "Something went wrong while generating tokens",
-    });
-  }
+export const issueTokenForUser = async (user: IUser) => {
+  const accessToken = user.generateAccessToken();
+  const refreshToken = user.generateRefreshToken();
+  user.refreshToken = refreshToken;
+  await user.save({ validateBeforeSave: false });
+  return { accessToken, refreshToken };
 };
