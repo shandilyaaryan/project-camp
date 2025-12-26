e67db5f1ddf25641856839d0cae9c49f0b79e8cb
Aryan <aryanshandilya10@gmail.com>
2025-12-26 12:08:47 +0530

    feat: added get user project route

--- src/routers/project.route.ts
+++ src/routers/project.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createProject } from "../controllers";
+import { createProject, getUserProject } from "../controllers";
 import { authMiddleware, validate } from "../middlewares";
 import { createProjectSchema } from "../validators";
 
 export const projectRouter = Router();
 
+projectRouter.get("/", authMiddleware, getUserProject);
 projectRouter.post(
   "/",
   authMiddleware,
